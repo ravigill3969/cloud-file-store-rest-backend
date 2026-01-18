87b046f824f4d2409e5c58d09451ab8571bded1e
ravigill3936 <gill1234ravi@gmail.com>
2026-01-18 18:43:48 -0500

    added db_url

--- database/connectDB.go
+++ database/connectDB.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
 	"log/slog"
 	"os"
 
@@ -11,16 +10,14 @@ import (
 
 func ConnectDB() (*sql.DB, error) {
 
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	db_url := os.Getenv("DATABASE_URL")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
+	// user := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// dbname := os.Getenv("DB_NAME")
 
-	psqlconn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname,
-	)
+	psqlconn := db_url
 
 	db, err := sql.Open("postgres", psqlconn)
 
