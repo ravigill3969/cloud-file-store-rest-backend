a3bfc8440d4acc1184511b2c63542d2c25cff8b7
ravigill3936 <gill1234ravi@gmail.com>
2026-02-20 14:31:39 -0500

    fixed routing bug

--- routes/file_routes.go
+++ routes/file_routes.go
@@ -5,6 +5,7 @@ import (
 
 	"backend/handlers"
 	middleware "backend/middlewares"
+
 	"github.com/redis/go-redis/v9"
 )
 
@@ -12,23 +13,23 @@ func FileRoutes(mux *http.ServeMux, fh *handlers.FileHandler, redis *redis.Clien
 	authMw := &middleware.RedisStruct{
 		RedisClient: redis,
 	}
-	mux.Handle("POST /api/file-store/file/edit/{id}/", authMw.AuthMiddleware(http.HandlerFunc(fh.HandleImageResizeRequestForUser)))
+	mux.Handle("POST /api/file/edit/{id}/", authMw.AuthMiddleware(http.HandlerFunc(fh.HandleImageResizeRequestForUser)))
 
-	mux.Handle("POST /api/file-store/file/upload", authMw.AuthMiddleware(http.HandlerFunc(fh.UploadFilesWithGoRoutines)))
-	mux.Handle("POST /api/file-store/media/upload", authMw.AuthMiddleware(http.HandlerFunc(fh.UploadMedia)))
+	mux.Handle("POST /api/file/upload", authMw.AuthMiddleware(http.HandlerFunc(fh.UploadFilesWithGoRoutines)))
+	mux.Handle("POST /api/media/upload", authMw.AuthMiddleware(http.HandlerFunc(fh.UploadMedia)))
 
-	mux.Handle("GET /api/file-store/file/get-all", authMw.AuthMiddleware(http.HandlerFunc(fh.GetAllUserFiles)))
-	mux.Handle("GET /api/file-store/file/download", authMw.AuthMiddleware(http.HandlerFunc(fh.DownloadFile)))
-	mux.Handle("GET /api/file-store/file/deleted-images", authMw.AuthMiddleware(http.HandlerFunc(fh.ListSoftDeletedImagesByUser)))
-	mux.Handle("PATCH /api/file-store/file/recover", authMw.AuthMiddleware(http.HandlerFunc(fh.RecoverDeletedImage)))
-	mux.Handle("DELETE /api/file-store/file/delete", authMw.AuthMiddleware(http.HandlerFunc(fh.DeleteImages)))
-	mux.Handle("DELETE /api/file-store/file/delete-permanently", authMw.AuthMiddleware(http.HandlerFunc(fh.HardDeleteSoftDeletedImage)))
+	mux.Handle("GET /api/file/get-all", authMw.AuthMiddleware(http.HandlerFunc(fh.GetAllUserFiles)))
+	mux.Handle("GET /api/file/download", authMw.AuthMiddleware(http.HandlerFunc(fh.DownloadFile)))
+	mux.Handle("GET /api/file/deleted-images", authMw.AuthMiddleware(http.HandlerFunc(fh.ListSoftDeletedImagesByUser)))
+	mux.Handle("PATCH /api/file/recover", authMw.AuthMiddleware(http.HandlerFunc(fh.RecoverDeletedImage)))
+	mux.Handle("DELETE /api/file/delete", authMw.AuthMiddleware(http.HandlerFunc(fh.DeleteImages)))
+	mux.Handle("DELETE /api/file/delete-permanently", authMw.AuthMiddleware(http.HandlerFunc(fh.HardDeleteSoftDeletedImage)))
 
-	// mux.HandleFunc("GET /api/file-store/file/get/{id}", fh.ServeFileWithIDForUI)
-	mux.HandleFunc("GET /api/file-store/file/get-file/{id}", fh.ServeFileWithIDForThirdParty)
-	mux.HandleFunc("POST /api/file-store/file/upload/{publicKey}/secure/{secretKey}", fh.UploadAsThirdParty)
-	mux.HandleFunc("POST /api/file-store/file/edit/{id}/{publicKey}/secure/{secretKey}", fh.HandleImageResizeRequestForThirdParty)
-	mux.HandleFunc("DELETE /api/file-store/file/delete/{id}/{publicKey}/secure/{secretKey}", fh.DeleteImageForThirdParty)
+	// mux.HandleFunc("GET /api/file/get/{id}", fh.ServeFileWithIDForUI)
+	mux.HandleFunc("GET /api/file/get-file/{id}", fh.ServeFileWithIDForThirdParty)
+	mux.HandleFunc("POST /api/file/upload/{publicKey}/secure/{secretKey}", fh.UploadAsThirdParty)
+	mux.HandleFunc("POST /api/file/edit/{id}/{publicKey}/secure/{secretKey}", fh.HandleImageResizeRequestForThirdParty)
+	mux.HandleFunc("DELETE /api/file/delete/{id}/{publicKey}/secure/{secretKey}", fh.DeleteImageForThirdParty)
 
 	mux.HandleFunc("POST /api/file-store/media/upload/{publicKey}/secure/{secretKey}", fh.UploadMediaForThirdParty)
 	// Video Routes
@@ -40,4 +41,3 @@ func FileRoutes(mux *http.ServeMux, fh *handlers.FileHandler, redis *redis.Clien
 	mux.HandleFunc("POST /api/file-store/video/upload/{publicKey}/secure/{secretKey}", fh.UploadVideoForThirdParty)
 	mux.HandleFunc("DELETE /api/file-store/video/delete/{publicKey}/secure/{secretKey}/{vid}", fh.DeleteVideoForThirdParty)
 }
-	
